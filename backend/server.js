005d6456efe3d898d8fe74b3521af1f353d9cdeb
rngustj9139 <mnb9139@khu.ac.kr>
2022-12-22 11:11:19 +0900

    Add comment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser'); // bodyParser는 클라이언트에서 오는 요청의 본문을 해석해주는 역할을 하는 라이브러리이다.
 
-const db = require('./db');
+const db = require('./db'); // db.js의 pool 이용
 
 const app = express();
 
-app.use(bodyParser.json()); // json 형태로 오는 요청의 본문을 해석해줄수있게 등록
+app.use(bodyParser.json()); // json 형태로 오는 요청의 본문을 해석해줄수있게 body-parser를 등록
 
 // 사용할 테이블 생성하기
 db.pool.query(`CREATE TABLE lists (
@@ -30,7 +30,7 @@ app.get('/api/values', function (req, res) {
 
 // 클라이언트에서 입력한 값을 데이터베이스 lists 테이블에 넣어주기
 app.post('/api/value', function (req, res, next) {
-    db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`,
+    db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`, // body-parser를 이용하기 때문에 클라이언트에서 오는 요청의 본문(req.body.value)을 쉽게 해석 가능
     (err, results, fields) => {
         if (err) {
             return res.status(500).send(err);
